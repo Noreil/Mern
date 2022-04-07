bacdbc140110668d3859e906d55a9d1865f2c235
Oreilleu <aurelien.picardpabijan@gmail.com>
2022-04-07 18:39:15 +0200

    e

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,4 @@
+const userModel = require('../models/user.model');
 const UserModel = require('../models/user.model');
 const ObjectID = require('mongoose').Types.ObjectId;
 
@@ -37,14 +38,47 @@ module.exports.deleteUser = (req, res) => {
 
 module.exports.follow = (req, res) => {
     // Add to the follower list
-    UserModel.findOne({_id: req.params.id }, { following: req.body.idToFollow })
-        .then(() => res.status(201).json({ message: 'Add to follower list '}))
-        .catch(err => res.status(400).json({ message : err }))
+    // UserModel.findOne({_id: req.params.id }, { following: req.body.idToFollow })
+    //     .then(() => res.status(201).json({ message: 'Add to follower list '}))
+    //     .catch(err => res.status(400).json({ message : err }))
+    
+    // Je veux que Indon like Aurélien donc :
+    // ID Indon add to followers Aurélien et 
+    // ID Aurélien add to followings Indon
+    userModel.findOne({ _id: req.params.id })
+        .then(user => {
+            if(!user.following.includes(req.body.idToFollow)) {
+                userModel.updateOne({ _id: req.params.id },
+                    {
+                        $push: {following: req.body.idToFollow}
+                    }
+                )
+                .then(() => res.status(201).json({ message : 'Ajout dans following'}))
+                .catch(err => res.status(400).json({ message : err }))   
+            }else {
+                res.status(401).send('Déja dans la liste de follow')
+            }
+            if(!user.followers.includes(req.params.id) && !user.following.includes(req.body.idToFollow)) {
+                userModel.updateOne({ _id: req.body.idToFollow },
+                    {
+                        $push: {followers: req.params.id}
+                    }
+                )
+                .then(() => res.status(201).json({ message : 'Ajout dans following'}))
+                .catch(err => res.status(400).json({ message : err }))   
+            }else {
+                res.status(401).send('Déja dans la liste de follow')
+            }
+        })
 
-    // Add to the following list
-    UserModel.findOne({_id: req.body.idToFollow }, { followers: req.params.id })
-        // .then(() => res.status(201).json({ message : 'Add to following list'}))
-        .catch(err => res.status(400).json({ message : err }))
+
+
+
+    
+    // // Add to the following list
+    // UserModel.findOne({_id: req.body.idToFollow }, { followers: req.params.id })
+    //     // .then(() => res.status(201).json({ message : 'Add to following list'}))
+    //     .catch(err => res.status(400).json({ message : err }))
 
 }
 
